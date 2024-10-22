647c913c9bb90a17907ad4b79a3b0d96465c8bc6
August Saint Freytag <me@augustfreytag.com>
2024-10-22 13:11:32 +0200

    Disable dev tools opening for renderer

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ const createWindow = () => {
 	})
 
 	// Open the DevTools.
-	mainWindow.webContents.openDevTools()
+	// mainWindow.webContents.openDevTools()
 
 	// and load the index.html of the app.
 	if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
